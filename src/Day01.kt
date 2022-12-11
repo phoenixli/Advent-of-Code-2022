9f206ab4b623b81914f5aa63b649c25e9140aa57
Huang Li <huangli@google.com>
2022-12-11 11:40:12 -0800

    Add Day01 solution.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,44 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCalories = 0
+        var currCalories = 0
+
+        input.forEach {
+            val foodCal = it.toIntOrNull()
+            if (foodCal != null) {
+                currCalories += foodCal
+            } else {
+                maxCalories = maxOf(currCalories, maxCalories)
+                currCalories = 0
+            }
+        }
+
+        return maxCalories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val allCalories = mutableListOf<Int>()
+        var currCalories = 0
+
+        input.forEach {
+            val foodCal = it.toIntOrNull()
+            if (foodCal != null) {
+                currCalories += foodCal
+            } else {
+                allCalories.add(currCalories)
+                currCalories = 0
+            }
+        }
+
+        return allCalories.sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
+
